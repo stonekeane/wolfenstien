57b1c90c685f3d1c01efea59794a149d16021491
stonekeane <mrstoneethankeane@gmail.com>
2023-04-27 18:13:28 -0500

    start npc

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ class Game:
 
     def draw(self):
         self.screen.fill("black")
-        self.object_renderer.draw()
-        self.weapon.draw()
-        #self.map.draw()
-        #self.player.draw()
+        # self.object_renderer.draw()
+        # self.weapon.draw()
+        self.map.draw()
+        self.player.draw()
 
     def check_events(self):
         for event in pg.event.get():
