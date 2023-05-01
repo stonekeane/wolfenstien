f6712078f48cfe918c2b5649ee23e04a29a194c3
stonekeane <mrstoneethankeane@gmail.com>
2023-05-01 11:21:32 -0500

    get raycasting working correctly

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ class Game:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_triger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
@@ -37,17 +40,20 @@ class Game:
         pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
 
     def draw(self):
-        self.screen.fill("black")
-        # self.object_renderer.draw()
-        # self.weapon.draw()
-        self.map.draw()
-        self.player.draw()
+        #self.screen.fill("black")
+        self.object_renderer.draw()
+        self.weapon.draw()
+        #self.map.draw()
+        #self.player.draw()
 
     def check_events(self):
+        self.global_triger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_triger = True
             self.player.single_fire_event(event)
 
     def run(self):
--- npc.py
+++ npc.py
@@ -1,3 +1,5 @@
+import math
+
 from sprite_object import *
 from random import randint, random, choice
 
@@ -20,7 +22,7 @@ class NPC(AnimatedSprite):
         self.alive = True
         self.pain = False
         self.ray_cast_value = False
-
+        self.frame_counter = False
 
     def update(self):
         self.check_animation_time()
@@ -28,25 +30,66 @@ class NPC(AnimatedSprite):
         self.run_logic()
         self.draw_ray_cast()
 
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx * self.size), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy * self.size)):
+            self.y += dy
+
+
+    def movement(self):
+        next_pos = self.game.player.map_pos
+        next_x, next_y = next_pos
+        angle = math.atan2(next_y + 0.5 - self.y, next_x + 0.5 - self.x)
+        dx = math.cos(angle) * self.speed
+        dy = math.sin(angle) * self.speed
+        self.check_wall_collision(dx, dy)
+
+    def animate_death(self):
+        if not self.alive:
+            if self.game.global_triger and self.frame_counter < len(self.death_images) -1:
+                self.death_images.rotate(-1)
+                self.image = self.death_images[0]
+                self.frame_counter += 1
+
     def animate_pain(self):
         self.animate(self.pain_images)
         if self.animation_trigger:
             self.pain = False
 
     def check_hit_in_npc(self):
-        if self.game.player.shot:
+        if self.ray_cast_value and self.game.player.shot:
             if HALF_WIDTH - self.sprite_half_width < self.screen_x < HALF_WIDTH + self.sprite_half_width:
                 self.game.sound.npc_pain.play()
                 self.game.player.shot = False
                 self.pain = True
+                self.health -= self.game.weapon.damage
+                self.check_health()
+
+    def check_health(self):
+        if self.health < 1:
+            self.alive = False
+            self.game.sound.npc_death.play()
 
     def run_logic(self):
         if self.alive:
+            self.ray_cast_value = self.ray_cast_player_npc()
             self.check_hit_in_npc()
+
             if self.pain:
                 self.animate_pain()
+
+            elif self.ray_cast_value:
+                self.animate(self.walk_images)
+                self.movement()
+
             else:
                 self.animate(self.idle_images)
+        else:
+            self.animate_death()
 
     @property
     def map_pos(self):
@@ -88,36 +131,37 @@ class NPC(AnimatedSprite):
             y_hor += dy
             depth_hor += delta_depth
 
-            # verticals
-            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+        # verticals
+        x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
 
-            depth_vert = (x_vert - ox) / cos_a
-            y_vert = oy + depth_vert * sin_a
+        depth_vert = (x_vert - ox) / cos_a
+        y_vert = oy + depth_vert * sin_a
 
-            delta_depth = dx / cos_a
-            dy = delta_depth * sin_a
-            for i in range(MAX_DEPTH):
-                tile_vert = int(x_vert), int(y_vert)
-                if tile_vert == self.map_pos:
-                    player_dist_v = depth_vert
-                    break
-                if tile_vert in self.game.map.world_map:
-                    wall_dist_v = depth_vert
-                    break
-                x_vert += dx
-                y_vert += dy
-                depth_vert += delta_depth
+        delta_depth = dx / cos_a
+        dy = delta_depth * sin_a
+        for i in range(MAX_DEPTH):
+            tile_vert = int(x_vert), int(y_vert)
+            if tile_vert == self.map_pos:
+                player_dist_v = depth_vert
+                break
+            if tile_vert in self.game.map.world_map:
+                wall_dist_v = depth_vert
+                break
+            x_vert += dx
+            y_vert += dy
+            depth_vert += delta_depth
 
-            player_dist = max(player_dist_v, player_dist_h)
-            wall_dist = max(wall_dist_v, wall_dist_h)
+        player_dist = max(player_dist_v, player_dist_h)
+        wall_dist = max(wall_dist_v, wall_dist_h)
 
-            if 0 < player_dist < wall_dist or not wall_dist:
-                return True
-            return False
+        if 0 < player_dist < wall_dist or not wall_dist:
+            return True
+        return False
 
     def draw_ray_cast(self):
-        pg.draw.circle(self.game.screen, 'red', (100 * self.x, 100 * self.y), 15)
-        if self.ray_cast_player_npc():
-            pg.draw.line(self.game.screen, 'orange', (100 * self.game.player.x, 100* self.game.player.y),
-                         (100 * self.x, 100 * self.y), 2)
-
+        pass
+        # pg.draw.circle(self.game.screen, 'red', (100 * self.x, 100 * self.y), 15)
+        # if self.ray_cast_player_npc():
+        #     pg.draw.line(self.game.screen, 'orange', (100 * self.game.player.x, 100* self.game.player.y),
+        #                  (100 * self.x, 100 * self.y), 2)
+        #
